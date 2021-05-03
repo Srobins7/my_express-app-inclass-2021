ac6234a0fcaef6fb6800102ad3b8c997c20ce967
Srobins7 <81520902+Srobins7@users.noreply.github.com>
2021-05-03 16:31:33 -0400

    Adding flash messages

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var expressLayouts = require('express-ejs-layouts'); 
+var expressLayouts = require('express-ejs-layouts');
+var session = require('express-session'); // around line 7
+var flash = require('express-flash-messages'); // around line 8 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var stocksRouter = require('./routes/stocks');
+
+var SESSION_SECRET = process.env.SESSION_SECRET || "super secret"
+
 var app = express();
 
 // view engine setup
@@ -21,6 +27,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  cookie: { maxAge: 60000},
+  secret: SESSION_SECRET,
+  name: 'stocks-app-session',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash())
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
